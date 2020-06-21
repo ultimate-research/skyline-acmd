00b525722de445b62a567263f2146ee0d12a8af9
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-06-21 13:37:18 -0400

    Add acmd_proc_macro, allow for names in function call arguments

---
+++ acmd_proc_macros/src/lib.rs
@@ -0,0 +1,78 @@
+use proc_macro::TokenStream;
+use syn::{Ident, Path, Expr, Token};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use quote::quote;
+
+#[derive(Debug)]
+struct FuncCall {
+    pub name: Path,
+    pub paren_token: syn::token::Paren,
+    pub args: Punctuated<ArgExpr, Token![,]>,
+}
+
+#[derive(Debug)]
+struct ArgExpr {
+    pub name: Option<Ident>,
+    pub expr: Expr,
+}
+
+impl Parse for ArgExpr {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.peek(Ident) && input.peek2(Token![=]) {
+            let name = input.parse()?;
+            let _: Token![=] = input.parse()?;
+            Ok(Self {
+                name: Some(name),
+                expr: input.parse()?
+            })
+        } else {
+            Ok(Self {
+                name: None,
+                expr: input.parse()?
+            })
+        }
+    }
+}
+
+impl Parse for FuncCall {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        Ok(Self {
+            name: input.parse()?,
+            paren_token: syn::parenthesized!(content in input),
+            args: content.parse_terminated(ArgExpr::parse)?,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn single_acmd_func(input: TokenStream) -> TokenStream {
+    let func_call = syn::parse_macro_input!(input as FuncCall);
+
+    if func_call.name.is_ident("frame") {
+        // frame
+        todo!()
+    } else if func_call.name.is_ident("wait") {
+        //wait
+        todo!()
+    } else if func_call.name.get_ident().is_some() {
+        // ACMD functions
+        let func_name = func_call.name;
+        let args = func_call.args.iter().map(|arg| arg.expr.clone());
+        quote!(
+            l2c_agent.clear_lua_stack();
+            #(
+                l2c_agent.push_lua_stack(&mut (#args).into());
+            )*
+            ::smash::app::sv_animcmd::#func_name(lua_state);
+        ).into()
+    } else if func_call.name.segments.iter().next().unwrap().ident
+                    .to_string().starts_with("sv_") {
+        // Lua calling convention
+        todo!()
+    } else {
+        // Module functions
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+pub use acmd_proc_macros::*;
+
+#[macro_export] macro_rules! acmd {
+    (
+        $l2c_state:expr,
+        {
+            $(
+                $func_name:ident(
+                    $($args:tt)*
+                );
+            )*
+        }
+    ) => {
+        let l2c_agent = &mut L2CAgent::new($l2c_state);
+        let lua_state = $l2c_state;
+
+        $(
+            acmd::single_acmd_func!($func_name ($($args)*));
+        )*
+    };
+}
+
 use smash::lua2cpp::L2CFighterCommon;
 
 pub fn add_hook(callback: fn(&mut L2CFighterCommon)) {
